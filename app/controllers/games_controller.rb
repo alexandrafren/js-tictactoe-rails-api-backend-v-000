7fbc0d466b6603b81ae6518770b00ef028fe4eb7
Alexandra Fren <alexandra.fren@gmail.com>
2019-01-30 17:39:58 +0000

    Automatically backed up by Learn

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,7 @@
 class GamesController < ApplicationController
   # Add your GamesController code here
   def create
-    @game = Game.create(game_params)
+    @game = Game.create(state)
     render json: @game.to_json
   end
 
